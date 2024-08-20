2d80d59f755eb9f714e54989d3db816de80563c0
alfygeorge <c0836170@mylambton.ca>
2024-08-20 15:06:06 -0400

    splash screen implemented

--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.movie
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import android.window.SplashScreen
+import androidx.annotation.RequiresApi
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.slideIn
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import java.time.Instant
+import kotlin.time.Duration
 
 class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        splashScreenAnim()
+
     }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun splashScreenAnim(){
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenView,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.height.toFloat()
+            )
+            slideUp.interpolator = AnticipateInterpolator()
+            slideUp.duration = 1000L
+
+            slideUp.doOnEnd { splashScreenView.remove() }
+
+            slideUp.start()
+        }
+    }
+
+
+
 }
